af0f50d25dd08d24c3d4606fbd468bb9dba73762
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-12-31 13:17:33 -0800

    change active link

--- components/ActiveLink.tsx
+++ components/ActiveLink.tsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'next/link'
 import React, { Children } from 'react'
 
-const ActiveLink = ({ children, activeClassName, ...props }) => {
+const ActiveLink = ({ children, activeClassName, ...props }:any) => {
   const { asPath } = useRouter()
   const child = Children.only(children)
   const childClassName = child.props.className || ''
