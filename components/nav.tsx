8b0a8307977560605f4772940bed18144599aa32
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-12-30 23:07:07 -0800

    new tab

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,20 +6,23 @@ const navigationPayroll = [
   },
   {
     'name': "List",
-    'url': "https://docs.google.com/spreadsheets/d/197-LIu3K9lBEFU8DD0uEmaQnbJUiTpCH9itwMA1aCs0/edit?usp=sharing"
+    'url': "https://docs.google.com/spreadsheets/d/197-LIu3K9lBEFU8DD0uEmaQnbJUiTpCH9itwMA1aCs0/edit?usp=sharing",
+    'newtab': true
   }
 ]
 
 function Nav() {
   return <div className="bg-[#1a1a1a] flex flex-col">
     <nav className="flex flex-row  h-content">
-      {navigationPayroll.map((item, itemIdx) =>
+      {navigationPayroll.map((item:any, itemIdx) =>
                      
               
                      <ActiveLink activeClassName="text-gray-100 py-3 px-6 block hover:text-green-300 focus:outline-none text-green-300 border-b-2 font-medium border-green-300" href={item.url}
                      key={itemIdx}
                      >
-                     <a className="text-gray-100 py-3 px-6 block hover:text-green-300 focus:outline-none">
+                     <a className="text-gray-100 py-3 px-6 block hover:text-green-300 focus:outline-none"
+                          target={`${item.newtab === true ? "_blank" : ""}`}
+                     >
                                                  {item.name}
                      </a>
                      </ActiveLink>
