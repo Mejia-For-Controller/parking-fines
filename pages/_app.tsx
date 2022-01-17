3af6163ac106397f729dc63c8e452d09347eb792
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-01-17 11:30:26 -0800

    tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,27 @@ import './mapboxdark.css';
 import TagManager from 'react-gtm-module'
 import { useEffect } from 'react';
 
+import { datadogRum } from '@datadog/browser-rum';
+    
 const tagManagerArgs = {
   gtmId: 'GTM-MQG62S5'
+}
+try {
+  datadogRum.init({
+    applicationId: '0f167d08-abab-4f76-bdc7-71efacca54d8',
+    clientToken: 'pub15407666c25ebb17ff50cdde4892057f',
+    site: 'datadoghq.com',
+    service:'mejia-users',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel: 'allow'
+  });
+    
+  datadogRum.startSessionReplayRecording();
+} catch (datadogerr) {
+  console.error(datadogerr)
 }
 
 
